d6085975b37874dd3cf44b289abd9c3e3c0bd6c8
Moulik Rai <raimoulik@gmail.com>
2023-05-11 16:47:37 +0545

    chore: add header component

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="header text-black py-2" style={{ background: "rgba(255, 255, 255, 0.9)" }}>
+      <div className="container flex justify-between items-center">
+        <div>logo</div>
+        <ul className="nav-list flex">
+          <li className="px-2">sadfa</li>
+          <li className="px-2">sadfa</li>
+          <li className="px-2">sadfa</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Header
