19eb6564f3e254fc83568d9e022da34e46afb64c
Moulik Rai <raimoulik@gmail.com>
2023-05-11 16:47:46 +0545

    chore: add social view

---
+++ src/app/views/Social.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import BlockHeader from '../components/BlockHeader'
+
+const Social = () => {
+  return (
+    <section id='social' className='bg-primary'>
+      <div className="container">
+        <BlockHeader>Social</BlockHeader>
+      </div>
+    </section>
+  )
+}
+
+export default Social
