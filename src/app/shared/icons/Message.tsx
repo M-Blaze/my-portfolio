66afdd1c446a9a9e93d68715fa532ecc4864efcd
Moulik Rai <raimoulik@gmail.com>
2023-05-12 08:39:24 +0545

    chore: build icon components

---
+++ src/app/shared/icons/Message.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgMessage = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width="1em"
+    height="1em"
+    fill="currentColor"
+    className="message_svg__x-el message_svg__x-el-svg message_svg__c2-1 message_svg__c2-2 message_svg__c2-q message_svg__c2-r message_svg__c2-s message_svg__c2-3 message_svg__c2-4 message_svg__c2-5 message_svg__c2-6 message_svg__c2-7 message_svg__c2-8"
+    data-ux="Icon"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <rect width={16} height={10.222} x={4} y={6} rx={1.129} />
+    <path d="m8.977 18.578.2-2.722a.564.564 0 0 1 .564-.523h3.61c.548 0 .774.705.327 1.024l-3.81 2.721a.564.564 0 0 1-.89-.5z" />
+  </svg>
+);
+export default SvgMessage;
--- src/app/shared/icons/index.ts
+++ src/app/shared/icons/index.ts
@@ -1,1 +1,6 @@
-export { default as IconDownload } from "./IconDownload";
+export { default as Download } from "./Download";
+export { default as Github } from "./Github";
+export { default as Gmail } from "./Gmail";
+export { default as Linkedin } from "./Linkedin";
+export { default as Message } from "./Message";
+export { default as Youtube } from "./Youtube";
