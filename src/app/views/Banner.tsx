e6fb2463a4f4de88ba053ade611ad710876de805
Moulik Rai <raimoulik@gmail.com>
2023-05-14 16:27:20 +0545

    update: make banner responsive

--- src/app/views/Banner.tsx
+++ src/app/views/Banner.tsx
@@ -8,10 +8,10 @@ const Banner = () => {
       <Image src="/banner.png" className='object-cover' alt="banner image" fill />
       <div className="container">
         <div className="banner-content flex justify-center items-center relative py-36">
-          <div className="rounded-full w-96 h-96 flex flex-wrap justify-center items-center" style={{ background: 'rgba(145, 213, 172, 0.9)' }}>
+          <div className="rounded-full w-64 h-64 tablet:w-96 tablet:h-96 flex flex-wrap justify-center items-center" style={{ background: 'rgba(145, 213, 172, 0.9)' }}>
             <div className="text-wrap text-center">
-              <h1 className='w-full text-5xl mb-4'>Software <br /> Engineer</h1>
-              <strong className='text-xl'>Develop with purpose</strong>
+              <h1 className='w-full text-2xl tablet:text-5xl mb-2 tablet:mb-4'>Software <br /> Engineer</h1>
+              <strong className='text-md tablet:text-xl'>Develop with purpose</strong>
             </div>
           </div>
         </div>
