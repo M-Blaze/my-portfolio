6a3f775fa1c947d5e39350551bb8056308529aea
Moulik Rai <raimoulik@gmail.com>
2023-05-22 22:44:10 +0545

    chore: add header in block page

--- src/app/Blogs/Blogs.tsx
+++ src/app/Blogs/Blogs.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Blogs = () => {
   return (
-    <div>Blogs</div>
+    <>
+      <div className="header-block pt-20 bg-yellow-400">
+        <div className="container">
+          <div className="text-wrap py-20">
+            <h1 className='text-8xl mb-4'>Stay curious.</h1>
+            <p className='text-2xl'>Discover stories, thinking, and expertise <br /> from writers on any topic.</p>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
