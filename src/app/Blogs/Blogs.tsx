ac399dd96136a0c120cc58d3798798cc1c761b6b
Moulik Rai <raimoulik@gmail.com>
2023-05-23 16:16:27 +0545

    chore: add subscribe button in blog

--- src/app/Blogs/Blogs.tsx
+++ src/app/Blogs/Blogs.tsx
@@ -1,13 +1,23 @@
+"use client"
 import React from 'react'
 
+import Button from '../components/Button'
+
 const Blogs = () => {
+  const subscribeHandler = () => {
+    console.log('clicked')
+  }
+
   return (
     <>
       <div className="header-block pt-20 bg-yellow-400">
         <div className="container">
-          <div className="text-wrap py-20">
+          <div className="content-wrap py-20">
             <h1 className='text-8xl mb-4'>Stay curious.</h1>
-            <p className='text-2xl'>Discover stories, thinking, and expertise <br /> from writers on any topic.</p>
+            <p className='text-2xl mb-8'>Discover stories, thinking, and expertise <br /> from writers on any topic.</p>
+            <div className="btn-holder">
+              <Button className="border-2 border-primary rounded-full px-8 py-2 text-xl font-bold bg-primary hover:bg-transparent text-yellow-400 hover:text-primary transition-all duration-200" onClick={subscribeHandler}>Subscribe</Button>
+            </div>
           </div>
         </div>
       </div>
