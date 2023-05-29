f3ae0253cf8545727634ab215ca753b85446470f
Moulik Rai <raimoulik@gmail.com>
2023-05-29 20:02:16 +0545

    chore: create Tag component

---
+++ src/app/components/Tag.tsx
@@ -0,0 +1,19 @@
+'use client'
+import React from 'react'
+import Link from 'next/link'
+
+import { Button } from '@chakra-ui/react'
+
+interface TagProps {
+  tag: string
+}
+
+const Tag:React.FC<TagProps> = ({ tag }) => {
+  return (
+    <Link className='mx-1' href={`/tag/${tag}`} key={tag}>
+      <Button size='sm' className='capitalize rounded-full px-4 bg-gray-100 font-normal'>{tag}</Button>
+    </Link>
+  )
+}
+
+export default Tag
